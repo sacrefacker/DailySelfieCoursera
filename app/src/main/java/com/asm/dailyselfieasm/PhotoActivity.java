a0c3cacd9742344fbe1f12de6470fc606c680be8
Ale <Ale@ASMultra>
2015-08-23 00:48:55 +0400

    Photo Fragment - doesn't retain

--- app/src/main/java/com/asm/dailyselfieasm/PhotoActivity.java
+++ app/src/main/java/com/asm/dailyselfieasm/PhotoActivity.java
@@ -6,30 +6,30 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class PhotoActivity extends Activity {
-    private FragmentManager fragmentManager;
+    private static final String TAG = "DailySelfieAsm";
+    private static final String TAG_PHOTO_FRAGMENT = "photo_fragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        fragmentManager = getFragmentManager();
-
         setContentView(R.layout.photo_activity);
 
         Intent incomingIntent = getIntent();
         String filename = incomingIntent.getStringExtra(MainActivity.EXTRA_BITMAP);
-        Toast.makeText(getApplicationContext(), R.string.please_wait, Toast.LENGTH_SHORT).show();
+        Log.i(TAG, filename + " is loaded from intent");
 
         Bundle args = new Bundle();
         args.putString(MainActivity.EXTRA_BITMAP, filename);
+        FragmentManager fragmentManager = getFragmentManager();
         PhotoFragment photoFragment = new PhotoFragment();
         photoFragment.setArguments(args);
 
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.photo_activity, photoFragment);
+        fragmentTransaction.replace(R.id.frame, photoFragment, TAG_PHOTO_FRAGMENT);
         fragmentTransaction.commit();
     }
 }
