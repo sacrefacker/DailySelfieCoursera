740c0e2d42aa0443d1efad5ea89d3fa3de024072
Ale <Ale@ASMultra>
2015-08-23 02:11:29 +0400

    Bitmap retains through orientation changes

--- app/src/main/java/com/asm/dailyselfieasm/PhotoActivity.java
+++ app/src/main/java/com/asm/dailyselfieasm/PhotoActivity.java
@@ -5,31 +5,77 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class PhotoActivity extends Activity {
+public class PhotoActivity extends Activity implements SetImageCallback {
     private static final String TAG = "DailySelfieAsm";
-    private static final String TAG_PHOTO_FRAGMENT = "photo_fragment";
+    public static final String IMAGE_BUNDLE = "image_bundle";
+
+    private ImageView photoView;
+    private Bitmap retainImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.photo_activity);
+        photoView = (ImageView)findViewById(R.id.imageView);
 
-        Intent incomingIntent = getIntent();
-        String filename = incomingIntent.getStringExtra(MainActivity.EXTRA_BITMAP);
-        Log.i(TAG, filename + " is loaded from intent");
+        if (null != savedInstanceState) {
+            retainImage = savedInstanceState.getParcelable(IMAGE_BUNDLE);
+            photoView.setImageBitmap(retainImage);
+        }
+        else {
 
-        Bundle args = new Bundle();
-        args.putString(MainActivity.EXTRA_BITMAP, filename);
-        FragmentManager fragmentManager = getFragmentManager();
-        PhotoFragment photoFragment = new PhotoFragment();
-        photoFragment.setArguments(args);
+            Intent incomingIntent = getIntent();
+            String filename = incomingIntent.getStringExtra(MainActivity.EXTRA_BITMAP);
+            Log.i(TAG, filename + " is loaded from intent");
 
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame, photoFragment, TAG_PHOTO_FRAGMENT);
-        fragmentTransaction.commit();
+            DiskAdapter.getInstance().loadImage(this, filename, false);
+            Toast.makeText(getApplicationContext(), R.string.please_wait, Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    // done TODO: don't load image from disk each time orientation changes
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(IMAGE_BUNDLE, retainImage);
+    }
+
+    @Override
+    public void setImage(Bitmap image, String filename) {
+        Log.i(TAG, "loaded photo");
+        runOnUiThread(new SetImage(image, filename));
+    }
+
+    private class SetImage implements Runnable {
+        private Bitmap image;
+        private String filename;
+
+        public SetImage(Bitmap image, String filename) {
+            this.image = image;
+            this.filename = filename;
+        }
+
+        @Override
+        public void run() {
+
+            if (null != photoView && null != image) {
+                photoView.setImageBitmap(image);
+                retainImage = image;
+                Toast.makeText(getApplicationContext(), filename, Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(getApplicationContext(), R.string.cannot_load, Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 }
