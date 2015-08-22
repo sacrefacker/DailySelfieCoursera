5830fa3fdc880e5ca0abf8affc4dcd139e3eccd1
Ale <Ale@ASMdesktop>
2015-08-22 19:17:34 +0400

    Photo fragment is almost working (image doesn't load)

--- app/src/main/java/com/asm/dailyselfieasm/PhotoActivity.java
+++ app/src/main/java/com/asm/dailyselfieasm/PhotoActivity.java
@@ -2,70 +2,34 @@ package com.asm.dailyselfieasm;
 
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.File;
-
-public class PhotoActivity extends Activity implements SetImageCallback{
-    private static final String TAG = "DailySelfieAsm";
-
-    ImageView photoView;
+public class PhotoActivity extends Activity {
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // TODO: landscape and portrait orientations
-        // Convert to fragments?
-        //
+        fragmentManager = getFragmentManager();
 
         setContentView(R.layout.photo_activity);
-        photoView = (ImageView) findViewById(R.id.imageView);
 
         Intent incomingIntent = getIntent();
         String filename = incomingIntent.getStringExtra(MainActivity.EXTRA_BITMAP);
-        DiskAdapter.getInstance().loadImage(this, filename, false);
         Toast.makeText(getApplicationContext(), R.string.please_wait, Toast.LENGTH_SHORT).show();
 
-        // TODO: deleting photo from here
-        // singleton / intent for result ?
-        //
+        Bundle args = new Bundle();
+        args.putString(MainActivity.EXTRA_BITMAP, filename);
+        PhotoFragment photoFragment = new PhotoFragment();
+        photoFragment.setArguments(args);
 
-    }
-
-    @Override
-    public void setImage(Bitmap image, String filename) {
-        Log.i(TAG, "loaded photo");
-        runOnUiThread(new SetImage(image, filename));
-    }
-
-    private class SetImage implements Runnable {
-        private Bitmap image;
-        private String filename;
-
-        public SetImage(Bitmap image, String filename) {
-            this.image = image;
-            this.filename = filename;
-        }
-
-        @Override
-        public void run() {
-
-            if (null != photoView && null != image) {
-                photoView.setImageBitmap(image);
-                Toast.makeText(getApplicationContext(), filename, Toast.LENGTH_SHORT).show();
-            }
-            else {
-                Toast.makeText(getApplicationContext(), R.string.cannot_load, Toast.LENGTH_SHORT).show();
-            }
-
-        }
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.photo_activity, photoFragment);
+        fragmentTransaction.commit();
     }
 }
